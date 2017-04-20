3ac1c64fbce4763925ef91d8db2011090ded10b5
Adrian Setyadi <a.styd@yahoo.com>
2017-04-20 23:05:25 +0700

    Add new API and reimplement sleeping

--- lib/sleep2.rb
+++ lib/sleep2.rb
@@ -3,34 +3,46 @@ require "sleep2/version"
 class Sleep2
   include Comparable
 
-  attr_accessor :time, :time_of_instantiation
+  def self.[] duration
+    new duration
+  end
 
-  def initialize time
-    @time_of_instantiation = Time.now
-    @time = time.to_f
+  attr_accessor :duration, :sleep2
+  attr_writer :new
+
+  def initialize duration
+    @duration = duration.to_f
+    @new      = true
+  end
+
+  def new?
+    @new
   end
 
   def inspect
     # Avoid sleeping the first time it is called (the moment of instantiation)
-    unless Time.now - time_of_instantiation <= 0.1
-      sleep time
+    unless new?
+      self.sleep2.call
+    else
+      self.new   = false
+      self.sleep2 = proc { sleep duration }
     end
   end
 
   # Make sleep instance can be calculated with integer
   [:*, :/, :+, :-, :%, :**].each do |m|
     define_method m do |num|
-      num = num.time if num.kind_of? self.class
-      sleep time.public_send(m, num)
+      num = num.duration if num.kind_of? self.class
+      self.class[ duration.public_send(m, num) ]
     end
   end
 
   # Make sleep instance comparable with another object of its type and integer
   def <=>(other)
     if other.kind_of? self.class
-      self.time <=> other.time
+      self.duration <=> other.duration
     elsif other.kind_of? Integer
-      self.time <=> other
+      self.duration <=> other
     end
   end
 end
